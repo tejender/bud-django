23a39aacada5f54ade581b4ff524ffaaee03a923
tejender <kumartejender007@gmail.com>
2022-12-14 15:36:37 +0530

    added more login features

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 # rooms = [
 #     {'id':1,'name':'deluxe room'},
@@ -30,7 +31,17 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room=room,
+            body = request.POST.get('body'))
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+    context = {'room':room,'room_messages':messages,'participants':participants}
     return render(request,'base/room.html',context)
 
 @login_required(login_url='login')
@@ -67,15 +78,18 @@ def deleteRoom(request,pk):
 
     if request.user != room.host:
         return HttpResponse('your are not authrized')
-        
+
     if request.method=='POST':
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
 
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method=='POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -92,9 +106,42 @@ def loginPage(request):
             messages.error(request,"Invalid credentials")
 
 
-    context={}
+    context={'page':page}
     return render(request,'base/login_register.html',context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    #  page = 'register'
+     if request.user.is_authenticated:
+        return redirect('home')
+
+     form = UserCreationForm
+     if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) # freeze save for cleanning data sent by user
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'error ! try again later')
+     context={'form':form}
+     return render(request,'base/login_register.html',context)
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+
+    if request.user != message.user:
+        return HttpResponse('your are not authrized to do that')
+
+    if request.method=='POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':message})
+
